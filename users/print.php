7d8872a2c637c9f37ad3a9a7f592432f35569f7d
izza iqbal <65022161+izzybug@users.noreply.github.com>
2023-11-25 20:52:08 +0700

    update v.0.7

--- users/print.php
+++ users/print.php
@@ -1,68 +1,92 @@
-<?php include('../includes/config.php'); ?>
+<?php include('../includes/config.php') ?>
 
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Print</title>
+<?php
+if (isset($_GET['id']) && ($_GET['id'] == 1 || $_GET['id'] == 2)) {
+    $id = $_GET['id'];
 
-	<!-- Custom fonts for this template-->
-	<link href="../src/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    // Define the SQL query based on the id parameter
+    $sqlQuery = ($id == 1) ? "SELECT * FROM daftar_istilah_medis" : "SELECT * FROM daftar_istilah_penanganan";
 
-	<style>
-		body {
-			font-family: Arial, sans-serif;
-		}
+    $result = mysqli_query($conn, $sqlQuery);
 
-		table {
-			width: 100%;
-			border-collapse: collapse;
-			margin-bottom: 20px;
-		}
+    if ($result) {
+        echo "
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <title>Print</title>
+            <!-- Custom fonts for this template-->
+            <link href='../src/fontawesome-free/css/all.min.css' rel='stylesheet' type='text/css'>
+            <style>
+                body {
+                    font-family: Arial, sans-serif;
+                }
+                table {
+                    width: 100%;
+                    border-collapse: collapse;
+                    margin-bottom: 20px;
+                }
+                th, td {
+                    border: 1px solid #ddd;
+                    padding: 8px;
+                    text-align: left;
+                }
+                th {
+                    background-color: #f2f2f2;
+                }
+                h2 {
+                    text-align: center;
+                }
+            </style>
+        </head>
+        <body>
+            <center>
+                <h2>DAFTAR ISTILAH MEDIS</h2>
+            </center>
+            <br>
+            <table>
+                <tr>
+                    <th width='1%'>No</th>
+                    <th>Istilah Medis</th>
+                    <th>Pembentukan Istilan Medis</th>
+                    <th>Arti</th>
+                </tr>";
 
-		th, td {
-			border: 1px solid #ddd;
-			padding: 8px;
-			text-align: left;
-		}
+        $no = 1;
+        while ($data = mysqli_fetch_array($result)) {
+            echo "
+            <tr>
+                <td>{$no}</td>
+                <td>{$data['istilah_medis']}</td>
+                <td>{$data['pembentukan_istilah_medis']}</td>
+                <td>{$data['arti']}</td>
+            </tr>";
+            $no++;
+        }
 
-		th {
-			background-color: #f2f2f2;
-		}
-
-		h2 {
-			text-align: center;
-		}
-	</style>
-</head>
-<body>
-	<center>
-		<h2>DAFTAR ISTILAH MEDIS</h2>
-	</center>
-	<br>
-	<table>
-		<tr>
-			<th width="1%">No</th>
-			<th>Istilah Medis</th>
-			<th>Pembentukan Istilan Medis</th>
-			<th>Arti</th>
-		</tr>
-		<?php
-		$no = 1;
-		$sql = mysqli_query($conn,"select * from daftar_istilah_medis");
-		while($data = mysqli_fetch_array($sql)){
-		?>
-		<tr>
-			<td><?php echo $no++; ?></td>
-			<td><?php echo $data['istilah_medis']; ?></td>
-			<td><?php echo $data['pembentukan_istilah_medis']; ?></td>
-			<td><?php echo $data['arti']; ?></td>
-		</tr>
-		<?php 
-		}
-		?>
-	</table>
-	<script>
-		window.print();
-	</script>
-</body>
-</html>
+        echo "
+            </table>
+            <script>
+                window.print();
+            </script>
+        </body>
+        </html>";
+    } else {
+        echo "Error in SQL query: " . mysqli_error($conn);
+    }
+} else {
+    echo "
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>Error</title>
+    </head>
+    <body>
+        <script>
+            alert('Invalid request.');
+            window.location.href = 'http://localhost/coding/project%20amel/admin/materi.php';
+        </script>
+    </body>
+    </html>";
+}
+?>
